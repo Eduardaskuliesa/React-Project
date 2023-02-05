b96a7e63379994a61ebbbf159c1293e736937567
Eduardaskuliesa <eduardas2000@mail.ru>
2023-02-05 22:30:16 +0200

    feat(5) Created Routs

--- Client-view/src/components/layouts/drawer-layouts.tsx
+++ Client-view/src/components/layouts/drawer-layouts.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Outlet } from 'react-router-dom';
 import IconButton from '../presentational/icon-button';
 import classes from './drawer-layouts.module.scss';
 import cls from '../../helpers/cls';
+import DrawerContent from '.';
 
-type DrawerLayoutProps = {
-  drawerContent: React.ReactNode,
-  children: React.ReactNode
-};
+const isHTMLElement = (el: EventTarget | null): el is HTMLElement => el instanceof HTMLElement;
 
-const DrawerLayout: React.FC<DrawerLayoutProps> = ({
-  drawerContent,
-  children,
-}) => {
+const DrawerLayout = () => {
   const [open, setOpen] = React.useState<boolean>(false);
+  const drawerHtmlElementRef = React.useRef<HTMLElement | null>(null);
 
   const drawerClasses = [classes.drawer];
   if (open) drawerClasses.push(classes.open);
@@ -21,19 +18,44 @@ const DrawerLayout: React.FC<DrawerLayoutProps> = ({
   const mainSectionClasses = [classes.main];
   if (open) mainSectionClasses.push(classes.open);
 
+  React.useEffect(() => {
+    const handleClickAway = (event: WindowEventMap['click']) => {
+      const drawerHtmlElement = drawerHtmlElementRef.current;
+
+      if (open && drawerHtmlElement !== null && isHTMLElement(event.target)) {
+        const targetDescendentOfDrawer = event.target.contains(drawerHtmlElement);
+        if (!targetDescendentOfDrawer) {
+          setOpen(false);
+        }
+      }
+    };
+
+    window.addEventListener('click', handleClickAway);
+
+    return () => {
+      window.removeEventListener('click', handleClickAway);
+    };
+  }, [open]);
+
   return (
     <div className={classes.layout}>
-      <aside className={cls(...drawerClasses)}>
-        {drawerContent}
+      <aside className={cls(...drawerClasses)} ref={drawerHtmlElementRef}>
+        <DrawerContent />
       </aside>
       <main className={cls(...mainSectionClasses)}>
-        {children}
+        <Outlet />
       </main>
       <div className={classes.toggler}>
-        <IconButton icon={open ? faXmark : faBars} onClick={() => setOpen(!open)} />
+        <IconButton
+          icon={open ? faXmark : faBars}
+          color={open ? 'white' : 'primary'}
+          onClick={(event) => {
+            event.stopPropagation();
+            setOpen(!open);
+          }}
+        />
       </div>
     </div>
   );
 };
-
 export default DrawerLayout;
